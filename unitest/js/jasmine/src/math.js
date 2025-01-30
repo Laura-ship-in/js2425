b95386b127d5359bc134b26d7e88f065e2de565e
Laura <elaura39@yahoo.com>
2025-01-30 20:47:39 +0200

    curs9

---
+++ unitest/js/jasmine/src/math.js
@@ -0,0 +1,16 @@
+function helloWorld() {
+    return "Hello World";
+}
+function sum (a, b) {
+    return "a + b";
+}
+function sum(a, b) {
+    a = a * 100;
+    b = b * 100;
+    let s = a+ b;
+    s = s / 100;
+    return Number(s.toFixed(2));
+}
+function dif(a, b) {
+    return
+}
